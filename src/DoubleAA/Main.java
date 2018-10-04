f859ec810004c911f69712c8179a1138ba9992a7
Anders <Andershaesling@gmail.com>
2018-10-04 14:04:34 +0200

    Initial commit

---
+++ src/DoubleAA/Main.java
@@ -0,0 +1,25 @@
+package DoubleAA;
+
+public class Main {
+
+    public static int getPentagonalNumber(int n){
+
+        // Opgave 6.1
+
+        int pentagon = (n*(3*n-1)/2);
+        return pentagon;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getPentagonalNumber(1));
+        System.out.println(getPentagonalNumber(2));
+        System.out.println(getPentagonalNumber(3));
+        System.out.println(getPentagonalNumber(4));
+
+
+
+
+    }
+
+}
